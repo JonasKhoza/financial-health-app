8c5ded1b01939a0eb25a7fb6c0fcfed072dd5b02
Jonas <jonaskhoza18@gmail.com>
2024-07-30 16:28:34 +0200

    Added the content of Navigation.tsx component, including styling it. Then I added a Pages directory to keep a pages of the website, and I added a page component called Authentication.tsx which is for both signup and ogin and I then created the Form folder in components,and the component Form.tsx in it to handle signup and signin data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-
+import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Layout from "./components/layout/Layout";
+
+const Authentication = lazy(async () => {
+  const module = await import("./pages/Authentication");
+  return { default: module.default };
+});
 
 function App() {
   return (
@@ -11,7 +15,23 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/test" element={<h1>Test</h1>} />
+          <Route
+            path="/signup"
+            element={
+              <Suspense fallback={<p>Loading!</p>}>
+                <Authentication />
+              </Suspense>
+            }
+          />
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route
+            path="/signin"
+            element={
+              <Suspense fallback={<p>Loading!</p>}>
+                <Authentication />
+              </Suspense>
+            }
+          />
           <Route path="/main" element={<h1>Main</h1>} />
           <Route path="/main2" element={<h1>Main</h1>} />
         </Routes>
--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,36 +1,57 @@
 import { NavLink } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
 
 import c from "./styles/navigation.module.css";
 
 const Navigation = () => {
   return (
-    <header className={c.header}>
-      <ul>
-        <li>
+    <header className={c.navigation_header}>
+      <div>
+        <p>
           <NavLink
             to="/"
-            className={({ isActive }) => (isActive ? "active" : "")}
+            className={({ isActive }) => (isActive ? c.active_tab : "")}
           >
-            Home
+            FinHealth
           </NavLink>
-        </li>
-        <li>
+        </p>
+      </div>
+      <div className={c.right_nav_container}>
+        <ul>
+          <li>
+            <NavLink
+              to="/dashboard"
+              className={({ isActive }) => (isActive ? c.active_tab : "")}
+            >
+              My Dashboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) => (isActive ? c.active_tab : "")}
+            >
+              Financial Health Quiz
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) => (isActive ? c.active_tab : "")}
+            >
+              Profile
+            </NavLink>
+          </li>
+        </ul>
+        <div className={c.sign_in_container}>
           <NavLink
-            to="/dashboard"
-            className={({ isActive }) => (isActive ? "active" : "")}
+            to="/login"
+            className={({ isActive }) => (isActive ? c.active_tab : "")}
           >
-            Dashboard
+            Sign in
           </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/profile"
-            className={({ isActive }) => (isActive ? "active" : "")}
-          >
-            Profile
-          </NavLink>
-        </li>
-      </ul>
+        </div>
+      </div>
     </header>
   );
 };
