3ef69f4c9a462455843ae37f2335e27be98a2fe6
Jonas <jonaskhoza18@gmail.com>
2024-08-10 20:06:54 +0200

    Set up the backend skeleton following the MVC pattern

---
+++ backend/src/server.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(cookieParser()); //allows to easily parse and manipulate HTTP cookies in your Express application.
+app.disable("x-powered-by"); //makes it more difficult for users to see that I am using Express
+
+express.urlencoded({
+  extended: true,
+}); /*to parse the URL-encoded form data sent in the request.
+When extended is set to true, the values can be of any type, 
+allowing for complex objects and arrays to be encoded in the URL-encoded format*/
+
+app.use(express.json()); //to parse incoming requests with JSON payloads.
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    "1": "Success",
+  });
+});
+
+const config = {
+  port: process.env.PORT || 8000,
+};
+
+app.listen(config.port, () =>
+  console.log("Successfully connected on port 8000")
+);
